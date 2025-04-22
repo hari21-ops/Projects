07b14ec672d607f69d5ce9aa8b6750db701c847c
hari21-ops <compgitht@yahoo.com>
2025-04-22 08:42:16 +0000

    Math logic and changes

---
+++ JS_Tutorial/math.js
@@ -0,0 +1,17 @@
+// MATH & NUM Tutorial: -
+//console.log(Math.ceil(4.2))
+//console.log(Math.ceil(4.7))
+
+//console.log(Math.floor(4.7));
+//console.log(Math.floor(4.3));
+
+//console.log(Math.random())
+//console.log(Math.random()*10)
+//console.log(Math.random()*10 + 1)
+//console.log((Math.random()*10) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (20 - 10 + 1)) + min)
+

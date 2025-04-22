2d9a4ed8755da5d073d610f9e6d8eda15bd18bf7
hari21-ops <compgitht@yahoo.com>
2025-04-22 06:10:18 +0000

    To understand JS_Data_Types_short code

---
+++ JS_Tutorial/js-data-types.js
@@ -0,0 +1,21 @@
+
+const score = 100
+//console.log(typeof score)
+
+const scoreValue = 100.456
+//console.log(typeof scoreValue)
+
+const login = true
+//console.log(typeof login)
+
+const isLoggedIn = false
+//console.log(typeof isLoggedIn)
+
+const outsideTemp = null
+//console.log(outsideTemp)
+//console.log("Type of var outside temp declared as null:" , typeof outsideTemp)
+
+const id = Symbol('123')
+const anotherSymbol = Symbol('123')
+console.log(id === anotherSymbol)
+

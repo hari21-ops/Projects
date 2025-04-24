cae1b6bc5b6f4fa932021b5bd79c71169184611e
hari21-ops <compgitht@yahoo.com>
2025-04-24 13:34:16 +0000

    this & arrow functions study

---
+++ JS_Tutorial/this-arrow-function.js
@@ -0,0 +1,57 @@
+/*
+const user = {
+    name: "pav-bhaji",
+    price: "130",
+
+    welcomeMessage: function(){
+        console.log(`${this.name}, Welcome to most favourite dish menu !`)
+    }
+}
+
+user.welcomeMessage()
+user.name = "Idli"
+user.welcomeMessage()
+console.log(this)
+
+function one(){
+    console.log(`Valus of this in function:`)
+    console.log(this)
+}
+
+one()
+
+
+const one = function() {
+    let name = "baby john"
+    console.log(this.username)
+}
+
+one()
+
+//Arrow Basic Function: -
+
+const addTwo = (num1, num2) => {
+    return num1 + num2
+}
+
+console.log(addTwo(3, 4))
+
+
+const addTwo = (num1, num2) => ( num1 + num2 )
+
+console.log(addTwo(98, 34))
+
+const addThree = (num1, num2) => ({ userName: "babyjohn"})
+
+console.log(addThree(9, 7))
+
+
+const myArray = [2, 5, 7]
+
+*/
+
+
+
+
+
+

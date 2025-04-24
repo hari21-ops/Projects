fea00754e597e561b4b56f8badd364b57b3b4b39
hari21-ops <compgitht@yahoo.com>
2025-04-24 08:46:10 +0000

    Function in JS

---
+++ JS_Tutorial/functions.js
@@ -0,0 +1,45 @@
+/* 
+function addNumbers(num1, num2){
+    return num1 + num2
+}
+
+// addNumbers  //Function reference 
+console.log(addNumbers(198, 647)) // Function execution
+
+
+function calculatePrice(var1, ...num1){
+    return num1
+}
+
+console.log(calculatePrice(100, 200, 500))
+*/
+
+/*
+const user = {
+    userName: "hitesh",
+    age: 28
+}
+
+function handleObject(anyobject){
+    console.log(`username is: ${anyobject.userName} and age is: ${anyobject.age}`);
+    
+}
+
+
+handleObject(user)
+handleObject({
+    userName: "sam",
+    age: 35
+})
+*/
+
+/*
+const myarray = [10, 20 , 30]
+
+function getArray(getArray){
+    return getArray[0]
+}
+
+console.log(getArray(myarray))
+*/
+
